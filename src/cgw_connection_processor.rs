bfab152797656b1918e9dcffd900a24998b95b57
outbackdingo <outbachdingo@gmail.com>
2024-03-20 09:28:23 +0700

    cargo fix --bin "ucentral-cgw" --allow-dirty
    Signed-off-by: outbackdingo <outbachdingo@gmail.com>

--- src/cgw_connection_processor.rs
+++ src/cgw_connection_processor.rs
@@ -144,7 +144,7 @@ fn cgw_parse_jrpc_event(map: &Map<String, Value>, method: String) -> CGWEvent {
 
 async fn cgw_process_jrpc_event(event: &CGWEvent) -> Result<(), String> {
     // TODO
-    if let CGWEvent::Connect(c) = event {
+    if let CGWEvent::Connect(_c) = event {
         /*
         info!(
             "Requesting {} to reboot (immediate request)",
@@ -314,7 +314,7 @@ impl CGWConnectionProcessor {
 
         let map = match cgw_process_jrpc_message(message).await {
             Ok(val) => val,
-            Err(e) => {
+            Err(_e) => {
                 error!("failed to recv connect message from {}, closing connection", self.addr);
                 return;
             }

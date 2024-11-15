64c00bbde49fb31f923b74671a896a12cb8b3ef3
Sviatoslav Boichuk <boichuk.sv@gmail.com>
2024-11-15 17:23:00 +0200

    Send infra_enqueue response if request enqueued succefully

--- src/cgw_connection_server.rs
+++ src/cgw_connection_server.rs
@@ -1905,32 +1905,31 @@ impl CGWConnectionServer {
             let queue_msg: CGWUCentralMessagesQueueItem =
                 CGWUCentralMessagesQueueItem::new(command, message, uuid, timeout);
             let queue_lock = CGW_MESSAGES_QUEUE.read().await;
-            match queue_lock.push_device_message(mac, queue_msg).await {
-                Ok(replaced_item) => {
-                    if let Some(req) = replaced_item {
-                        if let Ok(resp) = cgw_construct_infra_enqueue_response(
-                            self.local_cgw_id,
-                            req.uuid,
-                            false,
-                            Some("Request replaced with new!".to_string()),
-                        ) {
-                            self.enqueue_mbox_message_from_cgw_to_nb_api(infra_gid, resp);
-                        } else {
-                            error!("Failed to construct infra_request_result message!");
-                        }
-                    }
-                }
-                Err(e) => {
-                    if let Ok(resp) = cgw_construct_infra_enqueue_response(
+
+            let resp_result = match queue_lock.push_device_message(mac, queue_msg).await {
+                Ok(replaced_item) => match replaced_item {
+                    Some(req) => cgw_construct_infra_enqueue_response(
                         self.local_cgw_id,
-                        uuid,
+                        req.uuid,
                         false,
-                        Some(e.to_string()),
-                    ) {
-                        self.enqueue_mbox_message_from_cgw_to_nb_api(infra_gid, resp);
-                    } else {
-                        error!("Failed to construct infra_request_result message!");
+                        Some("Request replaced with new!".to_string()),
+                    ),
+                    None => {
+                        cgw_construct_infra_enqueue_response(self.local_cgw_id, uuid, true, None)
                     }
+                },
+                Err(e) => cgw_construct_infra_enqueue_response(
+                    self.local_cgw_id,
+                    uuid,
+                    false,
+                    Some(e.to_string()),
+                ),
+            };
+
+            match resp_result {
+                Ok(resp) => self.enqueue_mbox_message_from_cgw_to_nb_api(infra_gid, resp),
+                Err(e) => {
+                    error!("Failed to construct infra_request_result message! Error: {e}")
                 }
             }
         } else {
